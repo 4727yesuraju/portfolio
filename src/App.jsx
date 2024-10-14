9447d4528e3867fce8a99e66a99287556b297973
4727yesuraju <94819056+4727yesuraju@users.noreply.github.com>
2024-10-14 18:21:57 +0530

    basic scroll is added with scss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.scss'
+const App = () => {
+  return <div>
+    <section>Hero</section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>Parallax</section>
+    <section>p1</section>
+    <section>p2</section>
+    <section>p3</section>
+    <section>Contact</section>
+  </div>;
+};
+
+export default App;
